81ffeb391b8603eb67aad8d230e508af308c43d9
asdfzxcv782 <asdfzxcv7821@gmail.com>
2021-05-03 14:13:59 +0800

    send serverStatus to firestorage

--- routes/monitor.js
+++ routes/monitor.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const Db = require('../lib/firestore.js')
 
 router.get('/', function (req, res) {
   res.send("hello")
 })
 
 router.post('/',function(req, res){
+  let dateTime = Date.now().toString()
+  console.log(Date.now())
+  const status = Db.collection('serverStatus')
+  .doc(dateTime)
+
+  status.set(req.body)
   res.send(req.body)
 })
 
