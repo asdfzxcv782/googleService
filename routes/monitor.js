36da5e2a9c2ffd1d39f9e2aa699fac19a91a2756
asdfzxcv782 <asdfzxcv7821@gmail.com>
2021-05-07 17:11:46 +0800

    monitor update db  structure

--- routes/monitor.js
+++ routes/monitor.js
@@ -2,14 +2,16 @@ const express = require('express')
 const router = express.Router()
 const Db = require('../lib/firestore.js')
 
-router.get('/', function (req, res) {
-  const status = Db.collection("serverStatus")
+router.get('/:id', function (req, res) {
+  const status = Db.collection('Server')
+    .doc('Status')
+    .collection(req.params.id)
   status.get()
     .then((querySnapshot) => {
-      let data = []
+      const data = []
       querySnapshot.forEach((doc) => {
-        let result = doc.data()
-        let final = {}
+        const result = doc.data()
+        const final = {}
         final.cpuUsage = result.CpuUsage
         final.memoryFree = result.MemoryFree
         final.userName = result.userName
@@ -23,14 +25,37 @@ router.get('/', function (req, res) {
     })
 })
 
-router.post('/',function(req, res){
-  let dateTime = Date.now().toString()
-  console.log(Date.now())
-  const status = Db.collection('serverStatus')
-  .doc(dateTime)
+router.get('/instance', async function (req, res) {
+  const status = Db.collection('Server')
+    .doc('Status')
+  status.listCollections()
+    .then((querySnapshot) => {
+      const data = []
+      querySnapshot.forEach((doc) => {
+        data.push(doc.id)
+      })
+      return data
+    })
+    .then((data) => {
+      res.send(data)
+    })
+})
 
-  status.set(req.body)
-  res.send(req.body)
+router.post('/', function (req, res) {
+  const dateTime = Date.now().toString()
+  const data = req.body
+  data.createAt = dateTime
+  const status = Db.collection('Server')
+    .doc('Status')
+    .collection(data.userName)
+    .doc(dateTime)
+  status.set(data)
+    .then((data) => {
+      res.send(data)
+    })
+    .catch((err) => {
+      res.send('test: ' + err)
+    })
 })
 
 module.exports = router
